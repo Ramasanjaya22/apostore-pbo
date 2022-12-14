f30565c76dc22ba2fd18d42b156b2dfae4c0863d
Rama Sanjaya <ramasanjaya2202@gmail.com>
2022-12-14 09:02:31 +0700

    adding connection

--- src/controller/Conn.java
+++ src/controller/Conn.java
@@ -4,10 +4,26 @@
  */
 package controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Rama
  */
 public class Conn {
-    
+
+    public static Connection connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/apostore_db", "root", "root");
+            if (con != null) {
+                return con;
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", 2);
+        }
+        return null;
+    }
 }
